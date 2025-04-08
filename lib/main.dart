ae32d82d664a0e4fcc2f94c69887f8b66eebcf2b
mylanewby <167246119+mylanewby@users.noreply.github.com>
2025-04-07 23:38:49 -0400

    Added backend services + functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,13 @@
 import 'package:flutter/material.dart';
-/*
-  manage list of tasks with CRUD firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 
-  UI: 
-    Xtext input for task names
-    Xadd buttons 
-    single list view of tasks
-      each should have checkmark to mark as complete, delete to remove task, and nested list for sublist w additional tasks [daily+hourly tasks combined] 
-        ex. Monday list = 9-10AM details for that timeframe, Monday again with 12PM-2PM w another list of tasks(?)
-
-  Xstatefulwidget [tasklistscreen] = main screen
-    inside: list of tasks [obj w name, completion status, firebase-related info] as instance vars
-    methods to add/complete/remove tasks within tasklistscreen idget [incl firebase] 
-
-  firebase--ensure tasks add/complete/remove IRT
-    authentication to secure app and identify users
-
-  login and logout for users(?)
-
-  Monday list
-    9AM-10PM: 
-        HW1
-        Essay2
-    12PM-2PM: 
-        HW2
-        Essay4
-  Tuesday list
-    9AM-10PM: 
-      ex2
-      blah
-    12PM-2PM:
-      ex1
-      ex3
-
-  Overall task Name
-    Subtitle: 
-      task 
-      task
- */
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -83,23 +51,49 @@ class Task {
 class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _nameController = TextEditingController();
   List<TaskList> listList = [];
+  final CollectionReference _tasks = FirebaseFirestore.instance.collection('taskCollection');
 
-  void _addListItem() {
+  Future<void> _addListItem() async {
+    try {
+      await _tasks.doc(_nameController.text).set({
+        "subTasks": [],
+      });
+    } catch (e) {
+      print("Error adding document: $e");
+    }
     setState(() {
       listList.add(TaskList(
-        _nameController.text, 
+        _nameController.text,
         [],
       ));
     });
+    _nameController.text = "";
   }
   
-  void _deleteList(int index) {
+  void _deleteList(int index) async {
+    try {
+      await _tasks.doc(listList[index].listName).delete();
+    } catch (e) {
+      print("Error removing the task");
+    }
     setState(() {
       listList.removeAt(index);
     });
   }
 
-  void _addSubTask(int index) {
+  void _addSubTask(int index) async {
+    try {
+      await _tasks.doc(listList[index].listName).update({
+        "subTasks": FieldValue.arrayUnion([
+          {
+            "taskName": _nameController.text, 
+            "markComplete": false,
+          }
+        ])
+      });
+    } catch (e) {
+      print("Error with adding subtask");
+    }
     setState(() {
       listList[index].subTasks.add(
         Task(
@@ -107,9 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
           false,
       ));
     });
-    for (int i = 0; i <= listList[index].subTasks.length; i++) {
-      print(listList[index].subTasks[i].taskName);
-    }
+    _nameController.text = "";
   }
 
   @override
@@ -119,108 +111,160 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 150,
-              width: 250,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text("Add the list or subtask name here"),
-                  TextField(
-                    controller: _nameController,
-                    decoration: const InputDecoration(labelText: 'Name'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () { _addListItem(); },
-                    child: Text("Add List"),
-                  ),
-                ],
+      body: StreamBuilder<QuerySnapshot> (
+        stream: _tasks.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          listList.clear();
+          for (var doc in streamSnapshot.data!.docs) {
+            final data = doc.data() as Map<String, dynamic>;
+            final listName = doc.id;
+            final List<dynamic> subTasksData = data['subTasks'] ?? [];
+            List<Task> subTasks = subTasksData.map((subTask) {
+              return Task(
+                subTask['taskName'],
+                subTask['markComplete'],
+              );
+            }).toList();
+            listList.add(TaskList(listName, subTasks));
+          }
+          
+          return Center(child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 150,
+                width: 250,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text("Add the list or subtask name here"),
+                    TextField(
+                      controller: _nameController,
+                      decoration: const InputDecoration(labelText: 'Name'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () { _addListItem(); },
+                      child: Text("Add List"),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              height: 600,
-              child: ListView.builder(
-                itemCount: listList.length,
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            listList[index].listName,
-                          ),
-                          SizedBox(width: 30.0),
-                          SizedBox(
-                              child: ElevatedButton(
-                                onPressed: () { _deleteList(index); },
-                                child: Text('X'),
+              Container(
+                height: 600,
+                child: ListView.builder(
+                  itemCount: listList.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text( listList[index].listName ),
+                              SizedBox(width: 30.0),
+                              SizedBox(
+                                child: ElevatedButton(
+                                  onPressed: () { _deleteList(index); },
+                                  child: Text('X'),
+                                ),
                               ),
-                            ), 
-                          SizedBox(
-                            child: ElevatedButton(
-                              onPressed: () { _addSubTask(index); },
-                              child: Text('+ task'),
+                              SizedBox(
+                                child: ElevatedButton(
+                                  onPressed: () { _addSubTask(index); },
+                                  child: Text('+ task'),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            height: 100, 
+                            child: ListView.builder(
+                              shrinkWrap: true, 
+                              physics: ClampingScrollPhysics(),
+                              itemCount: listList[index].subTasks.length, 
+                              itemBuilder: (context, subIndex) {
+                                return SizedBox(
+                                  height: 40,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Checkbox(
+                                          value: listList[index].subTasks[subIndex].markComplete,
+                                          onChanged: (bool? value) async {
+                                            final task = listList[index].subTasks[subIndex];
+                                            final oldMap = {
+                                              "taskName": task.taskName,
+                                              "markComplete": task.markComplete,
+                                            };
+                                            final newMap = {
+                                              "taskName": task.taskName,
+                                              "markComplete": value!,
+                                            };
+
+                                            try {
+                                              await _tasks.doc(listList[index].listName).update({
+                                                "subTasks": FieldValue.arrayRemove([oldMap])
+                                              });
+                                              await _tasks.doc(listList[index].listName).update({
+                                                "subTasks": FieldValue.arrayUnion([newMap])
+                                              });
+
+                                              setState(() {
+                                                task.markComplete = value;
+                                              });
+                                            } catch (e) {
+                                              print("Error updating subtask: $e");
+                                            }
+                                            setState(() {
+                                              listList[index].subTasks[subIndex].markComplete = value!;
+                                            });
+                                          },
+                                        ),
+                                      Text(
+                                        listList[index].subTasks[subIndex].taskName,
+                                        style: TextStyle(
+                                          color: listList[index].subTasks[subIndex].markComplete 
+                                          ? Colors.grey : Colors.black,
+                                        ),
+                                      ),
+                                      SizedBox(width: 30.0),
+                                      SizedBox(
+                                          child: ElevatedButton(
+                                            onPressed: () async { 
+                                              try {
+                                                await _tasks.doc(listList[index].listName).update({
+                                                  "subTasks": FieldValue.arrayRemove([
+                                                    {
+                                                      "taskName": listList[index].subTasks[subIndex].taskName, 
+                                                      "markComplete": listList[index].subTasks[subIndex].markComplete,
+                                                    }
+                                                  ])
+                                                });
+                                              } catch (e) {
+                                                print("Error deleting subtask");
+                                              }
+                                              setState(() {
+                                                listList[index].subTasks.removeAt(subIndex);
+                                              });
+                                            },
+                                            child: Text('X'),
+                                          ),
+                                        ), 
+                                    ],
+                                  ),
+                                );
+                              }
                             ),
                           ),
                         ],
-                      ),
-                      Container(
-                        height: 100, 
-                        child: ListView.builder(
-                          shrinkWrap: true, 
-                          physics: ClampingScrollPhysics(),
-                          itemCount: listList[index].subTasks.length, 
-                          itemBuilder: (context, subIndex) {
-                            return SizedBox(
-                              height: 40,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Checkbox(
-                                      value: listList[index].subTasks[subIndex].markComplete,
-                                      onChanged: (bool? value) {
-                                        setState(() {
-                                          listList[index].subTasks[subIndex].markComplete = value!;
-                                        });
-                                      },
-                                    ),
-                                  Text(
-                                    listList[index].subTasks[subIndex].taskName,
-                                    style: TextStyle(
-                                      color: listList[index].subTasks[subIndex].markComplete 
-                                      ? Colors.grey : Colors.black,
-                                    ),
-                                  ),
-                                  SizedBox(width: 30.0),
-                                  SizedBox(
-                                      child: ElevatedButton(
-                                        onPressed: () { 
-                                          setState(() {
-                                            listList[index].subTasks.removeAt(subIndex);
-                                          });
-                                        },
-                                        child: Text('X'),
-                                      ),
-                                    ), 
-                                ],
-                              ),
-                            );
-                          }
-                        ),
-                      ),
-                    ],
-                  );
-                },
+                      );
+                  },
+                  ),
               ),
-            ),
-          ],
-        ),
+            ],//widget
+          ),//column
+        );
+        }
       ),
-    );
+    );//scaffold
   }
 }
